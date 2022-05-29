0663c5464d7c0cbb6588ea54bb398fc7171f9bdd
elif4423 <15290063@firat.edu.tr>
2022-05-29 20:57:43 +0300

    Film Ekleme İşlemleri

--- Sinema Otomasyonu/Sinema Otomasyonu/filmekle.Designer.cs
+++ Sinema Otomasyonu/Sinema Otomasyonu/filmekle.Designer.cs
@@ -28,19 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.filmnameTxt = new System.Windows.Forms.TextBox();
+            this.yonetmenTxt = new System.Windows.Forms.TextBox();
+            this.timeTxt = new System.Windows.Forms.TextBox();
+            this.yearTxt = new System.Windows.Forms.TextBox();
+            this.filmSave = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.turBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(103, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FİLM ADI :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(84, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "YÖNETMEN :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(88, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "FİLM TÜRÜ :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(77, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "FİLM SÜRESİ :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(92, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "YAPIM YILI :";
+            // 
+            // filmnameTxt
+            // 
+            this.filmnameTxt.Location = new System.Drawing.Point(183, 136);
+            this.filmnameTxt.Name = "filmnameTxt";
+            this.filmnameTxt.Size = new System.Drawing.Size(255, 23);
+            this.filmnameTxt.TabIndex = 5;
+            // 
+            // yonetmenTxt
+            // 
+            this.yonetmenTxt.Location = new System.Drawing.Point(183, 169);
+            this.yonetmenTxt.Name = "yonetmenTxt";
+            this.yonetmenTxt.Size = new System.Drawing.Size(255, 23);
+            this.yonetmenTxt.TabIndex = 6;
+            // 
+            // timeTxt
+            // 
+            this.timeTxt.Location = new System.Drawing.Point(183, 242);
+            this.timeTxt.Name = "timeTxt";
+            this.timeTxt.Size = new System.Drawing.Size(255, 23);
+            this.timeTxt.TabIndex = 7;
+            // 
+            // yearTxt
+            // 
+            this.yearTxt.Location = new System.Drawing.Point(183, 276);
+            this.yearTxt.Name = "yearTxt";
+            this.yearTxt.Size = new System.Drawing.Size(255, 23);
+            this.yearTxt.TabIndex = 8;
+            // 
+            // filmSave
+            // 
+            this.filmSave.BackColor = System.Drawing.Color.SpringGreen;
+            this.filmSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.filmSave.Location = new System.Drawing.Point(248, 325);
+            this.filmSave.Name = "filmSave";
+            this.filmSave.Size = new System.Drawing.Size(190, 46);
+            this.filmSave.TabIndex = 9;
+            this.filmSave.Text = "KAYDET";
+            this.filmSave.UseVisualStyleBackColor = false;
+            this.filmSave.Click += new System.EventHandler(this.filmSave_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(82, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 37);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "FİLM EKLE";
+            // 
+            // turBox
+            // 
+            this.turBox.FormattingEnabled = true;
+            this.turBox.Location = new System.Drawing.Point(183, 208);
+            this.turBox.Name = "turBox";
+            this.turBox.Size = new System.Drawing.Size(255, 23);
+            this.turBox.TabIndex = 11;
             // 
             // filmekle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(562, 472);
+            this.Controls.Add(this.turBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.filmSave);
+            this.Controls.Add(this.yearTxt);
+            this.Controls.Add(this.timeTxt);
+            this.Controls.Add(this.yonetmenTxt);
+            this.Controls.Add(this.filmnameTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "filmekle";
             this.Text = "filmekle";
+            this.Load += new System.EventHandler(this.filmekle_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox filmnameTxt;
+        private TextBox yonetmenTxt;
+        private TextBox timeTxt;
+        private TextBox yearTxt;
+        private Button filmSave;
+        private Label label6;
+        private ComboBox turBox;
     }
 }
--- Sinema Otomasyonu/Sinema Otomasyonu/filmekle.cs
+++ Sinema Otomasyonu/Sinema Otomasyonu/filmekle.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Data.OleDb;
 
 namespace Sinema_Otomasyonu
 {
@@ -15,6 +18,48 @@ namespace Sinema_Otomasyonu
         public filmekle()
         {
             InitializeComponent();
+        }
+
+        OleDbConnection baglantim = new OleDbConnection("Provider=Microsoft.Ace.OleDb.12.0;Data Source=data/database.accdb");
+
+        private void filmekle_Load(object sender, EventArgs e)
+        {
+            turBox.Items.Add("Seçiniz...");
+            turBox.Items.Add("Aksiyon");
+            turBox.Items.Add("Komedi");
+            turBox.Items.Add("Fantezi");
+            turBox.Items.Add("Korku");
+            turBox.Items.Add("Aşk");
+            turBox.Items.Add("Bilim Kurgu");
+
+            turBox.SelectedIndex = 0;
+        }
+
+        private void filmSave_Click(object sender, EventArgs e)
+        {
+            if (filmnameTxt.Text!="" && yonetmenTxt.Text!="" && turBox.SelectedItem.ToString()!= "Seçiniz..." && timeTxt.Text!="" && yearTxt.Text!="")
+            {
+                try
+                {
+                    baglantim.Open();
+                    OleDbCommand salonSave = new OleDbCommand("INSERT INTO film (filmAdi,yonetmen,filmturu,filmsuresi,yapimyili) VALUES('" + filmnameTxt.Text + "','" + yonetmenTxt.Text + "','" + turBox.SelectedItem.ToString() + "','" + timeTxt.Text + "','" + yearTxt.Text+"')", baglantim);
+                    salonSave.ExecuteNonQuery();
+                    baglantim.Close();
+                    MessageBox.Show(filmnameTxt.Text + " isimli film başarı ile kaydedildi !", "Sinema Otomasyonu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+                catch (Exception msg)
+                {
+
+                    MessageBox.Show(msg.Message, "Sinema Otomasyonu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    baglantim.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Boş Alan Bırakmayınız !", "Sinema Otomasyonu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
         }
     }
 }
